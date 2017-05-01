90a65f8177c98109e7f60f21a04b06dfe9da8a38
Zui <zuikng@gmail.com>
2017-05-01 16:37:00 -0400

    Add links to Nav bar

--- zui/src/Header.js
+++ zui/src/Header.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-
+import {Link} from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-          <h1>Hello, world</h1>
+      <div className="header">
+          <h1>Blah blahblah</h1>
+          <Navigation />
       </div>
     );
   }
 }
+
+class Navigation extends Component {
+  render() {
+    return (
+    <div className="nav">
+      <ul>
+        <li><Link to={"/about/"}>About</Link></li>
+        <li><Link to={"/projects/"}>Projects</Link></li>
+        <li><Link to={"/thoughts"}>Thoughts</Link></li>
+      </ul>
+    </div>);
+  }
+}
+
 
 export default Header;
--- zui/src/Home.js
+++ zui/src/Home.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './Header';
-import './Home.scss';
+import './Home.css';
 
 class Home extends Component {
   render() {
-    return (
+    return (<Router>
       <div className="container">
           <Header />
-          <h1>Hello, world</h1>
+          <Route path="/about/" component={About} />
+          <Route path="/projects/" component={Project} />
+          <Route path="/thoughts/" component={Thoughts} />
       </div>
+      </Router>
+    );
+  }
+}
+
+
+class About extends Component {
+  render() {
+    return (
+    <p>Content content content blah blah blah blahContent content content blah blah blah blahContent content content blah blah blah blahContent content content blah blah blah blahContent content content blah blah blah blah</p>
+    );
+  }
+}
+
+class Project extends Component {
+  render() {
+    return (
+      <h2>My projects would go here instead</h2>
+    );
+  }
+}
+
+class Thoughts extends Component {
+  render() {
+    return (
+      <h2>My blog</h2>
     );
   }
 }
