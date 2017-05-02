0f7c2a2ddf23667f4c5d52c43046c296c8b3a567
Duy Nguyen <zuikng@gmail.com>
2017-05-01 23:00:27 -0400

    Extract zui to other repo

--- zui/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
-import './index.css';
-
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-);
